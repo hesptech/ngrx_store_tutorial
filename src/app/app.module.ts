99fb9fef0ee72ad39ea55af2f35236ea03bd35a1
Oswaldo <olea.oswaldo@gmail.com>
2020-09-08 14:42:52 +0200

    all changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+// import { reducers, metaReducers } from './reducers';
+import { reducer } from './reducers/tutorial.reducer';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    /* StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }) */
+    StoreModule.forRoot({
+      tutorial: reducer
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
